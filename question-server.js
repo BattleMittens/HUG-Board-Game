92163e7cfa9964b2c03ff85477b72afd62453e34
Troy <battlemitten@gmail.com>
2019-05-05 19:37:00 -0400

    Yada 2 questions and me breaking my stuff
    Grrrr I'm mad

--- question-server.js
+++ question-server.js
@@ -15,6 +15,34 @@ function nextQuestion()
     // some logic to get me a question
     // I don't really care how it works just get me a question
 
+
+    const fs = require('fs') 
+  
+    // Reading data in utf-8 format 
+    // which is a type of character set. 
+    // Instead of 'utf-8' it can be  
+    // other character set also like 'ascii' 
+    fs.readFile('Dormammu.txt', 'utf-8', (err, data) =>
+    { 
+        if (err) throw err; 
+    
+        // Converting Raw Buffer to text 
+        // data using tostring function. 
+
+        console.log(data); 
+    })
+
+    var lineReader = require('readline').createInterface({
+        input: require('fs').createReadStream('file.in')
+      });
+      
+      lineReader.on('line', function (line) {
+        console.log('Line from file:', line);
+      });
+
+    let container = [];
+    container.push(question);
+
     let question =
     {
         question: 'What was the year Jamestown was founded?',
